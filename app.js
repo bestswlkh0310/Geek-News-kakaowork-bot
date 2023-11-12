2bdc6cbc3890615aeb834c53dad25a53cabd0abd
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2023-11-12 15:36:35 +0900

    Clean

--- app.js
+++ app.js
@@ -13,36 +13,36 @@ const octokit = new Octokit({
   auth: githubToken,
 });
 
+var pre = [];
+
 async function getNews() {
   const result = [];
 
-  for (let i = 1; i <= 50; i++) {
-    try {
-      const response = await axios.get(`https://news.hada.io/new?page=${i}`);
-      const $ = cheerio.load(response.data);
+  try {
+    const response = await axios.get(`https://news.hada.io/new?page=1`);
+    const $ = cheerio.load(response.data);
 
-      const topics = $('.topics').eq(0).find('.topic_row');
-      if (topics.length === 0) {
-        break;
-      }
-
-      topics.each((index, element) => {
-        const title = $(element).find('.topictitle').eq(0).text();
-        const description = $(element).find('.topicdesc').eq(0).find('a').eq(0).text();
-        const url = $(element).find('a').eq(1).attr('href');
-        const createdAt = $(element).find('.topicinfo').eq(0).text().split(" ")[4];
-
-        result.push({
-          id: i,
-          title: title,
-          description: description,
-          url: url,
-          createdAt: createdAt
-        });
-      });
-    } catch (error) {
-      console.log(error);
+    const topics = $('.topics').eq(0).find('.topic_row');
+    if (topics.length === 0) {
+      return []
     }
+
+    topics.each((index, element) => {
+      const title = $(element).find('.topictitle').eq(0).text();
+      const description = $(element).find('.topicdesc').eq(0).find('a').eq(0).text();
+      const url = $(element).find('a').eq(1).attr('href');
+      const createdAt = $(element).find('.topicinfo').eq(0).text().split(" ")[4];
+
+      result.push({
+        id: index,
+        title: title,
+        description: description,
+        url: url,
+        createdAt: createdAt
+      });
+    });
+  } catch (error) {
+    console.log(error);
   }
   return result;
 }
@@ -53,14 +53,31 @@ setInterval(async () => {
     const currentTime = new Date();
     const currentDateTime = `${currentTime.getFullYear()}년${currentTime.getMonth()}월${currentTime.getDate()}일${currentTime.getHours()}시${currentTime.getMinutes()}분${currentTime.getSeconds()}초`;
     const fileName = `data/${currentDateTime}.txt`;
-    const commitMessage = `${currentDateTime} 커밋`;
 
     var news = await getNews();
-    console.log(news.length);
 
-    const content = JSON.stringify(news);
+    const filtedNews = news.filter(e => {
+      t = true
+      pre.forEach(i => {
+        if (i.url == e.url) {
+          t = false
+        }
+      })
+      return t
+    })
 
-    console.log(content)
+    console.log('filted', filtedNews.length)
+    if (filtedNews.length == 0) {
+      return
+    }
+
+    var commitMessage = ':: 새로운 뉴스!\n';
+    filtedNews.forEach((i, idx) => {
+      commitMessage += `${idx + 1}. ${i.title}\n`
+      commitMessage += `- ${i.url}\n\n`
+    })
+r
+    pre = news
 
     // 파일을 생성하거나 업데이트
     const response = await octokit.repos.createOrUpdateFileContents({
@@ -68,14 +85,14 @@ setInterval(async () => {
       repo: repoName,
       path: fileName,
       message: commitMessage,
-      content: Buffer.from(content).toString('base64'),
+      content: "",
     });
 
     console.log(`Commit created: ${response.data.commit.message}`);
   } catch (error) {
     console.error(`An error occurred: ${error.message}`);
   }
-}, 3000); // 10분
+}, 5000); // 10분
 
 app.listen(3003, () => {
   console.log('start')
